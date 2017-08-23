0d01fbd0978444e8ec8bff4c0d65f599071e4877
kp201760 <30428224+kp201760@users.noreply.github.com>
2017-08-23 12:09:06 +1000

    make a default constructor for AdhocTicketFactory

--- bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -7,6 +7,7 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
 	  
 	}
 
+
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
 		// TODO Auto-generated method stub
